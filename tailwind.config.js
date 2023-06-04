0fbb452f1326cacd97d1eb470c0a75e2f278982b
SantoshThapaa <st708611@gmail.com>
2023-06-04 09:25:12 +0545

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "color-primary":"#692E21",
+        "color-dark":"#020726",
+        "color-silver":"#797D7F"
+      }
+    },
+  },
+  plugins: [],
+}
+
